53cff652151f8a360b44985c11ad6fcef3654898
Asolinmaso <150114681+Asolinmaso@users.noreply.github.com>
2025-12-17 12:21:28 +0530

    Update env.js

--- src/env.js
+++ src/env.js
@@ -11,7 +11,7 @@ function mustGet(name) {
 const env = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: Number(process.env.PORT || 4000),
-  MONGODB_URI: mustGet("MONGODB_URI"),
+  MONGODB_URI: process.env.MONGODB_URI || mustGet("MONGO_URI"),
   CORS_ORIGIN: process.env.CORS_ORIGIN || "http://localhost:3000",
 }
 
